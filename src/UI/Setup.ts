255c64454c1d076c36de2f06b69a8e5cf84b0387
vie_bcit <vienna_ly@bcit.ca>
2026-01-20 21:19:14 -0800

    Reset scene

--- src/UI/Setup.ts
+++ src/UI/Setup.ts
@@ -1,9 +1,11 @@
 import $ from "jquery";
+import {UnityResetScene} from "../utils/UnityLoaderSetup";
 import { QuizUISetUp } from "./QuizSetUp";
 
 
 export function SetupUI() {
     const header = `<div id="header" class="mmd instructions">
+        <button id="reset-scene-button">Reset Scene</button>&nbsp;&nbsp;
         <b>Select </b> <span class="icon mouse_lf" title="Use mouse on objects to interact with items."></span>
         <b>Pan </b> <span class="icon mouse_lf" title="Click & drag the to pan camera."></span> or arrow keys 
         <b>Rotate </b> <span class="icon mouse_rt" title="Right mouse click & drag to orbit scene."></span> or A,D 
@@ -12,8 +14,9 @@ export function SetupUI() {
       
     $("body").prepend($(header));    
 
-   
-    
+    $("#reset-scene-button").on("click", function() {
+        UnityResetScene();
+    });
     // Check for ?quiz=true in the URL, enable quiz UI if present
     const urlParams = new URLSearchParams(window.location.search);
     const quizParam = urlParams.get('quiz');
