43f167a9d2bcff4afc6930bfd6b6f85da283fa14
vie_bcit <vienna_ly@bcit.ca>
2026-01-20 19:06:35 -0800

    Unity update: dropzones, dropables; Revert Mouse controls for panning (added conflict resolution for drag)

--- src/UI/Setup.ts
+++ src/UI/Setup.ts
@@ -4,10 +4,10 @@ import { QuizUISetUp } from "./QuizSetUp";
 
 export function SetupUI() {
     const header = `<div id="header" class="mmd instructions">
-        <b>Select </b> <span class="icon mouse_lf" title="Left mouse click on objects to select them."></span>
+        <b>Select </b> <span class="icon mouse_lf" title="Use mouse on objects to interact with items."></span>
+        <b>Pan </b> <span class="icon mouse_lf" title="Click & drag the to pan camera."></span> or arrow keys 
         <b>Rotate </b> <span class="icon mouse_rt" title="Right mouse click & drag to orbit scene."></span> or A,D 
         <b>Zoom </b> <span class="icon mouse_wheel" title="Use scroll wheel to zoom."></span> or W,S
-        <b>Pan </b> <span class="icon mouse_wheel" title="Click & drag the middle button to pan camera."></span> or arrow keys 
         </div>`;
       
     $("body").prepend($(header));    
